c56db85d9f208f019cafaaeb201ef1cc89644b10
Hristo Borisov <hristo@foresightprudence.com>
2019-02-02 11:25:36 +0200

    DataBinding enabled

--- app/src/main/java/com/example/githubconnections/MainActivity.kt
+++ app/src/main/java/com/example/githubconnections/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.githubconnections
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import com.example.githubconnections.api.GitHubService
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.githubconnections.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 // TODO remove this code which was used for testing purposes
 //        val service = Retrofit.Builder()
 //            .baseUrl("https://api.github.com/")
--- app/src/main/java/com/example/githubconnections/ui/userslist/UsersListFragment.kt
+++ app/src/main/java/com/example/githubconnections/ui/userslist/UsersListFragment.kt
@@ -2,30 +2,23 @@ package com.example.githubconnections.ui.userslist
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.githubconnections.R
+import com.example.githubconnections.databinding.FragmentUsersListBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class UsersListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_list, container, false)
+        val dataBinding =
+            DataBindingUtil.inflate<FragmentUsersListBinding>(inflater, R.layout.fragment_users_list, container, false)
+        return dataBinding.root
     }
 
 
